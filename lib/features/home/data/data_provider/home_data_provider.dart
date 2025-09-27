cf7d951f078ab370aa52c66c835959390d055555
amala <amala@chargemod.com>
2025-09-28 00:38:27 +0530

    final commit

--- lib/features/home/data/data_provider/home_data_provider.dart
+++ lib/features/home/data/data_provider/home_data_provider.dart
@@ -3,13 +3,7 @@ import 'package:http/http.dart' as http;
 class HomeDataProvider {
   Future<http.Response> getProductList() async {
     try {
-      final res = http.get(
-        Uri.parse('https://fakestoreapi.com/products'),
-        headers: {
-          "Content-Type": "application/json",
-          "Accept": "application/json",
-        },
-      );
+      final res = http.get(Uri.parse('https://fakestoreapi.com/products'));
       return res;
     } catch (e) {
       throw e.toString();
